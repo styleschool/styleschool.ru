353e7895ff47de48961e58fcdaa71da9fad6069e
Пётр Безденежных <petrbezdeneznyh@MacBook-Pro-Petr.local>
2024-03-15 19:02:29 +0400

    default next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-const nextConfig = (phase, { defaultConfig }) => {
-    if (phase === PHASE_DEVELOPMENT_SERVER) {
-        return {
-            output: 'export',
-            images: { unoptimized: true },
-        }
-    } else {
-        return {
-            output: 'export',
-            images: { unoptimized: true },
-        }
-    }
+const nextConfig = (phase, { defaultConfig }) => { return defaultConfig
 }
 
 module.exports = nextConfig
