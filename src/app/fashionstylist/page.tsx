f0a9a25ca52077355a6d2efd54c171eb0f4ad51b
Пётр Безденежных <petrbezdeneznyh@MacBook-Pro-Petr.local>
2024-03-30 10:39:09 +0400

    redirects for advertisement

---
+++ src/app/fashionstylist/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function Fashionstylist() {  
+  redirect('/education/fashion-stylist');
+}
---
+++ src/app/polzovatelskoe_soglashenie/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function Orginfo() {  
+  redirect('/documents/orginfo.docx');
+}
